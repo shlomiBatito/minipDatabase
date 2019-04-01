f9a8b3d1faa2bfe93dba7dea8e73dbc1b71ffce1
NathAsco <45246668+NathAsco@users.noreply.github.com>
2019-04-01 19:57:20 +0300

    Add files via upload

---
+++ FilmSQL.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Films
+(
+  name INT NOT NULL,
+  genre INT NOT NULL,
+  rating INT NOT NULL,
+  releaseDate INT NOT NULL,
+  removalDate INT NOT NULL,
+  releaseYear INT NOT NULL,
+  movieId INT NOT NULL,
+  PRIMARY KEY (movieId)
+);
